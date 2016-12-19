bea3363b2f99d603f3a557c26cc1e860645349a1
FKJW068 <xsge@vip.qq.com>
2016-12-19 19:21:19 +0900

    多文件上传修正

--- app/Http/Controllers/AjaxController.php
+++ app/Http/Controllers/AjaxController.php
@@ -18,6 +18,14 @@ class AjaxController extends Controller
     public function upload(Request $request){
         $baidupcs = new BaiduPCS(Cookie::get('access_token'));
         $remote_dir = config('app.bapppath');
+        if(is_array($_FILES['file']['tmp_name'])){
+            var_dump($_FILES['file']['tmp_name']);
+            exit;
+            foreach ($_FILES['file']['tmp_name'] as $file){
+                var_dump($file);
+            }
+        }
+        exit;
         $local_path="C:\Users\FKJW068\Documents\ipmsg_img\ipmsgclip_r_1481690537_0.png";
         $file_name = basename($local_path);
         $file_size = filesize($local_path);
