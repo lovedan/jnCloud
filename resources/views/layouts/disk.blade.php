c03c0b59175121169fe458a598abea340e77c5c9
FKJW068 <xsge@vip.qq.com>
2016-12-22 16:33:39 +0900

    修改主页面logo样式

--- resources/views/layouts/disk.blade.php
+++ resources/views/layouts/disk.blade.php
@@ -42,7 +42,9 @@
             <!-- mini logo for sidebar mini 50x50 pixels -->
             <span class="logo-mini"><b>JN</b></span>
             <!-- logo for regular state and mobile devices -->
-            <span class="logo-lg"><b>江南云</b></span>
+            <span class="logo-lg">
+                <img src="{{ URL::asset('asset/images/logo.png') }}" width="130">
+            </span>
         </a>
         <!-- Header Navbar: style can be found in header.less -->
         <nav class="navbar navbar-static-top">
